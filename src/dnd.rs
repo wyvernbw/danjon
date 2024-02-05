69d5b30d13bef0c69668b6ffcf9eba5a7fb3be35
wyvernbw <iamwyvern511@gmail.com>
2024-02-05 21:52:18 +0200

    move prompt functions to their respective struct impls

--- src/dnd.rs
+++ src/dnd.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use strum::{Display, EnumIter};
+use strum::{Display, EnumIter, IntoEnumIterator};
+
+use crate::prelude::*;
 
 #[derive(Default, Debug, EnumIter, Display, Clone, PartialEq, Eq, Copy)]
 pub enum Dice {
@@ -12,6 +14,19 @@ pub enum Dice {
     D12,
     D20,
     Other(u8),
+}
+
+impl Dice {
+    pub fn prompt(prompt: &str) -> anyhow::Result<Dice> {
+        let dice = select(prompt, Dice::iter().collect())?;
+        let dice = match dice {
+            Dice::Other(_) => input("Enter the number of sides on your custom dice")
+                .and_then(|s| s.parse::<u8>().anyhow())
+                .map(Dice::Other)?,
+            dice => dice,
+        };
+        Ok(dice)
+    }
 }
 
 #[derive(EnumIter, Display, Debug, Clone, PartialEq, Eq)]
@@ -33,4 +48,23 @@ pub enum Class {
         name: Option<Arc<str>>,
         hit_dice: Dice,
     },
+}
+
+impl Class {
+    pub fn prompt() -> anyhow::Result<Class> {
+        let class = select("Select a class", Class::iter().collect())?;
+        let class = match class {
+            Class::Homebrew { .. } => {
+                let dice = Dice::prompt("Select the hit dice for your homebrew class")?;
+                let name = input("Enter the name of your homebrew class")?;
+                let name = arc_str(name);
+                Class::Homebrew {
+                    name: Some(name),
+                    hit_dice: dice,
+                }
+            }
+            _ => class,
+        };
+        Ok(class)
+    }
 }
--- src/hp.rs
+++ src/hp.rs
@@ -1,38 +1,7 @@
-use strum::IntoEnumIterator;
-
-use crate::dnd::{Class, Dice};
-use crate::prelude::*;
+use crate::dnd::Class;
 
 pub fn calculate_hp() -> anyhow::Result<()> {
-    let class = prompt_class()?;
+    let class = Class::prompt();
     tracing::info!(?class);
     Ok(())
-}
-
-pub fn prompt_class() -> anyhow::Result<Class> {
-    let class = select("Select a class", Class::iter().collect())?;
-    let class = match class {
-        Class::Homebrew { .. } => {
-            let dice = prompt_dice("Select the hit dice for your homebrew class")?;
-            let name = input("Enter the name of your homebrew class")?;
-            let name = arc_str(name);
-            Class::Homebrew {
-                name: Some(name),
-                hit_dice: dice,
-            }
-        }
-        _ => class,
-    };
-    Ok(class)
-}
-
-pub fn prompt_dice(prompt: &str) -> anyhow::Result<Dice> {
-    let dice = select(prompt, Dice::iter().collect())?;
-    let dice = match dice {
-        Dice::Other(_) => input("Enter the number of sides on your custom dice")
-            .and_then(|s| s.parse::<u8>().anyhow())
-            .map(Dice::Other)?,
-        dice => dice,
-    };
-    Ok(dice)
 }
